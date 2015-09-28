609351b3bd758f453568118dd7a23f35d41b4aaa
Maciek Pitucha <m.pitucha@camlintechnologies.com>
2015-09-28 15:08:09 +0200

    react hot replace

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,31 @@
+var webpack = require('webpack');
+
 module.exports = {
   context: __dirname + "/client",
-  entry: {
-    javascript: "./js/main.js",
-    // html: "./index.html",
-  },
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:3001', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server',
+    "./js/main.js"
+  ],
+  // entry: {
+  //   javascript: "./js/main.js",
+  //   // html: "./index.html",
+  // },
   output: {
     filename: "./js/bundle.js",
     path: __dirname + "/client",
   },
   module: {
-      loaders: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loaders: ["react-hot", "babel-loader"],          
-        },
-        // {
-        //   test: /\.html$/,
-        //   loader: "file?name=[name].[ext]",
-        // }
-      ],
-    },
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ["react-hot", "babel-loader"],          
+      }
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
 }
